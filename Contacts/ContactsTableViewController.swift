c09df8d8bf2cf574e041214a313fccc60e7d1e59
retina <retina@Joabe.local>
2019-01-31 10:46:26 -0200

    Loading data to table view cell

--- Contacts/ContactsTableViewController.swift
+++ Contacts/ContactsTableViewController.swift
@@ -33,23 +33,24 @@ class ContactsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return contactList.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let contact = contactList[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath) as! ContactsTableViewCell
 
-        // Configure the cell...
-
+        cell.buildCell(contact: contact, position: indexPath.row)
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
